5925870005b84ebc34534f8eca793b3350e5e75d
jotachango <jfchangop@gmail.com>
2025-08-20 10:36:04 -0500

    cleaning code VIII

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -50,8 +50,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={require("./CV_Juan_Chango_Android.pdf")}
+                    download="JotaChangoResume.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
