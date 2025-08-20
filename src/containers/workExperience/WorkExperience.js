bbeb769818376d5df53168c5d21083e50b8a146c
jotachango <jfchangop@gmail.com>
2025-08-20 10:40:08 -0500

    cleaning code IX

--- src/containers/workExperience/WorkExperience.js
+++ src/containers/workExperience/WorkExperience.js
@@ -10,29 +10,29 @@ export default function WorkExperience() {
   if (workExperiences.display) {
     return (
       <div id="experience">
-          <div className="experience-container" id="workExperience">
-            <div>
-              <h1 className="experience-heading">Experiences</h1>
-              <div className="experience-cards-div">
-                {workExperiences.experience.map((card, i) => {
-                  return (
-                    <ExperienceCard
-                      key={i}
-                      isDark={isDark}
-                      cardInfo={{
-                        company: card.company,
-                        desc: card.desc,
-                        date: card.date,
-                        companylogo: card.companylogo,
-                        role: card.role,
-                        descBullets: card.descBullets
-                      }}
-                    />
-                  );
-                })}
-              </div>
+        <div className="experience-container" id="workExperience">
+          <div>
+            <h1 className="experience-heading">Experiences</h1>
+            <div className="experience-cards-div">
+              {workExperiences.experience.map((card, i) => {
+                return (
+                  <ExperienceCard
+                    key={i}
+                    isDark={isDark}
+                    cardInfo={{
+                      company: card.company,
+                      desc: card.desc,
+                      date: card.date,
+                      companylogo: card.companylogo,
+                      role: card.role,
+                      descBullets: card.descBullets
+                    }}
+                  />
+                );
+              })}
             </div>
           </div>
+        </div>
       </div>
     );
   }
