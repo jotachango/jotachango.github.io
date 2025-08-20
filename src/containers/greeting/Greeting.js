edac2cd5b09e5599e015ab0f229e8d1e5ab143f2
jotachango <jfchangop@gmail.com>
2025-08-20 09:03:20 -0500

    solving some issues

---
+++ src/containers/greeting/Greeting.js
@@ -0,0 +1,77 @@
+import React, {useContext} from "react";
+import {Fade} from "react-reveal";
+import "./Greeting.scss";
+import landingPerson from "../../assets/lottie/landingPerson";
+import profilePicture from "../../assets/images/profile_2022.png";
+import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import SocialMedia from "../../components/socialMedia/SocialMedia";
+import Button from "../../components/button/Button";
+import {illustration, greeting} from "../../portfolio";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function Greeting() {
+  const {isDark} = useContext(StyleContext);
+  if (!greeting.displayGreeting) {
+    return null;
+  }
+  return (
+    <Fade bottom duration={1000} distance="40px">
+      <div className="greet-main" id="greeting">
+        <div className="greeting-main">
+          <div className="greeting-text-div">
+            <div>
+              <div className="greeting-title-image">
+                <img
+                  src={profilePicture}
+                  className="avatar"
+                  alt="Juan Chango"
+                />
+                <h1
+                  className={
+                    isDark ? "dark-mode greeting-text" : "greeting-text"
+                  }
+                >
+                  {greeting.title}
+                </h1>
+              </div>
+
+              <p
+                className={
+                  isDark
+                    ? "dark-mode greeting-text-p"
+                    : "greeting-text-p subTitle"
+                }
+              >
+                {greeting.subTitle}
+              </p>
+              <div id="resume" className="empty-div"></div>
+              <SocialMedia />
+              <div className="button-greeting-div">
+                <Button text="Contact me" href="#contact" />
+                {greeting.resumeLink && (
+                  <a
+                    href={require("./resume.pdf")}
+                    download="Resume.pdf"
+                    className="download-link-button"
+                  >
+                    <Button text="Download my resume" />
+                  </a>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="greeting-image-div">
+            {illustration.animated ? (
+              <DisplayLottie animationData={landingPerson} />
+            ) : (
+              <img
+                alt="man sitting on table"
+                src={require("../../assets/images/manOnTable.svg")}
+              ></img>
+            )}
+          </div>
+        </div>
+      </div>
+    </Fade>
+  );
+}
